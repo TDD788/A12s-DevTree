5a3754763b907adf9712b854cf19d03806978ea5
TheDarkDeath788 <103203291+TDD788@users.noreply.github.com>
2024-02-17 23:42:34 -0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,13 @@
+# Mkbootimage
+sudo apt install nano
+git clone https://gitlab.com/EdwinT2/avb_tool -b main out/host/linux-x86/bin
+sudo chmod +rwx out/host/linux-x86/bin/avbtool
+chmod a+x device/samsung/a12s/mkbootimg
+add_lunch_combo twrp_a12s-eng
+
+
+
+
 # General Configurations
 export ALLOW_MISSING_DEPENDENCIES=true
 export OF_DONT_PATCH_ENCRYPTED_DEVICE=1
