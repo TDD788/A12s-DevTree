b34f1aa1e576b4a623802aa31bbb9def5e84850c
TheDarkDeath788 <thedarkdeath788@gmail.com>
2024-03-14 22:07:00 -0300

    Fix Missing Separator

--- device.mk
+++ device.mk
@@ -5,7 +5,7 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # Fastbootd
 PRODUCT_PACKAGES += \
-    android.hardware.fastboot@1.0-impl-mock
+    android.hardware.fastboot@1.0-impl-mock \
     fastbootd
     #android.hardware.fastboot@1.0-impl-mock.recovery \
 
