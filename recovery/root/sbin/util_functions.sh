0c8762d2861a67669e740396c44e75f02fb7937f
TheDarkDeath788 <thedarkdeath788@gmail.com>
2024-03-28 17:14:43 -0300

    Fix Some Things

--- recovery/root/sbin/util_functions.sh
+++ recovery/root/sbin/util_functions.sh
@@ -14,7 +14,7 @@ grep_prop() {
   local REGEX="s/^$1=//p"
   shift
   local FILES=$@
-  [ -z "$FILES" ] && FILES='/system/build.prop'
+  [ -z "$FILES" ] && FILES='/system_root/system/build.prop'
   cat $FILES 2>/dev/null | dos2unix | sed -n "$REGEX" | head -n 1
 }
 
