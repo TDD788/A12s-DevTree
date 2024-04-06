4866dbb056ea4e6eef7e236c545943e34674b117
TheDarkDeath788 <103203291+TDD788@users.noreply.github.com>
2024-04-05 22:37:08 -0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -91,6 +91,10 @@ export_build_vars(){
 	# maximum permissible splash image size
 	# (in kilobytes); do *NOT* increase!
 	export OF_SPLASH_MAX_SIZE=128
+
+        #Target Device
+        export FOX_TARGET_DEVICES="a12s, a12sub, SM-A127M, SM-A127F, Fox_a12s"
+        export FOX_BUILD_DEVICE="a12s"
 	
 	# let's see what are our build VARs
 	if [ -n "$FOX_BUILD_LOG_FILE" -a -f "$FOX_BUILD_LOG_FILE" ]; then
