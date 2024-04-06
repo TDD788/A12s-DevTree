82143facf9d33f398fe4f6ffb2003c2daf0d2d2e
TheDarkDeath788 <thedarkdeath788@gmail.com>
2024-04-06 00:13:40 -0300

    Fixing SGDISK

--- twrp_a12s.mk
+++ twrp_a12s.mk
@@ -35,7 +35,6 @@ PRODUCT_PACKAGES += \
 
 - PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,$(DEVICE_PATH)/recovery/root,recovery/root)
 + PRODUCT_COPY_FILES += $(call cc_prebuilt_binary, parted)
-+ PRODUCT_COPY_FILES += $(call cc_prebuilt_binary, sgdisk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := a12s
