9b56cabb159a8eb0138c05927b4bdeeb45e55929
TheDarkDeath788 <103203291+TDD788@users.noreply.github.com>
2024-04-06 09:01:31 -0300

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,7 +3,7 @@ sudo apt install nano
 git clone https://gitlab.com/EdwinT2/avb_tool -b main out/host/linux-x86/bin
 sudo chmod +rwx out/host/linux-x86/bin/avbtool
 chmod a+x device/samsung/a12s/prebuilt/avb/mkbootimg
-add_lunch_combo twrp_a12s-eng
+COMMON_LUNCH_CHOICES := twrp_a12s-eng
 
 FDEVICE1="a12s"
 CURR_DEVICE="a12s"
