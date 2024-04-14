6f62701711c885a061563c86063e5e2db198e51b
TheDarkDeath788 <103203291+TDD788@users.noreply.github.com>
2024-04-14 15:43:37 -0300

    Update postrecoveryboot.sh

--- recovery/root/system/bin/postrecoveryboot.sh
+++ recovery/root/system/bin/postrecoveryboot.sh
@@ -6,6 +6,8 @@ xz -d /system/lib/libiconv.so.xz
 xz -d /system/lib/libandroid-support.so.xz
 xz -d /system/lib/libgmp.so.xz
 
+export LD_LIBRARY_PATH=/system/lib/
+
 coreutils_dir="/system/bin"
 target_dir="/system/bin/"
 coreutils_programs="b2sum base32 base64 basename basenc cat chcon chgrp chmod chown chroot cksum comm cp csplit cut date dd dir dircolors dirname du echo env expand expr factor false fmt fold ginstall groups head id join kill link ln logname ls md5sum mkdir mkfifo mknod mktemp mv nice nl nohup nproc numfmt od paste pathchk pr printenv printf ptx pwd readlink realpath rm rmdir runcon seq sha1sum sha224sum sha256sum sha384sum sha512sum shred shuf sleep sort split stat stdbuf stty sum sync tac tail tee test timeout touch tr true truncate tsort tty uname unexpand uniq unlink vdir wc whoami yes"
