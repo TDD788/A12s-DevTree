4d9e3d69ff974406a79ed2caf32187b58d136447
TheDarkDeath788 <103203291+TDD788@users.noreply.github.com>
2024-05-30 17:48:08 -0300

    Update bootimg.mk

--- prebuilt/avb/bootimg.mk
+++ prebuilt/avb/bootimg.mk
@@ -9,22 +9,17 @@ MKBOOTIMG := device/samsung/a12s/prebuilt/avb/mkbootimg
 
 FLASH_IMAGE_TARGET ?= $(PRODUCT_OUT)/recovery.tar
 
-LZ4 := lz4
 
-COMPRESSED_KERNEL := $(PRODUCT_OUT)/compressed_kernel.lz4
+FLASH_IMAGE_TARGET ?= $(PRODUCT_OUT)/recovery.tar
 
-$(COMPRESSED_KERNEL): $(call bootimage-to-kernel,$(1))
-	@echo "----- Compressing kernel with lz4 ------"
-	$(hide) $(LZ4) -f $(call bootimage-to-kernel,$(1)) $@
-
-$(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(COMPRESSED_KERNEL) $(INTERNAL_BOOTIMAGE_FILES) $(BOOTIMAGE_EXTRA_DEPS)
+$(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(BOOTIMAGE_EXTRA_DEPS)
 	$(call pretty,"Target boot image: $@")
-	$(hide) $(MKBOOTIMG) --kernel $(COMPRESSED_KERNEL) $(INTERNAL_BOOTIMAGE_ARGS) $(INTERNAL_MKBOOTIMG_VERSION_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
+	$(hide) $(MKBOOTIMG) --kernel $(call bootimage-to-kernel,$(1)) $(INTERNAL_BOOTIMAGE_ARGS) $(INTERNAL_MKBOOTIMG_VERSION_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
 	$(hide) echo -n "SEANDROIDENFORCE" >> $@
 	$(hide) $(call assert-max-image-size,$@,$(BOARD_BOOTIMAGE_PARTITION_SIZE),raw)
 	@echo "Made boot image: $@"
 
-$(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(COMPRESSED_KERNEL) $(recovery_ramdisk) $(RECOVERYIMAGE_EXTRA_DEPS)
+$(INSTALLED_RECOVERYIMAGE_TARGET): $(MKBOOTIMG) $(recovery_ramdisk) $(recovery_kernel) $(RECOVERYIMAGE_EXTRA_DEPS)
 	@echo "----- Making recovery image ------"
 	$(hide) $(MKBOOTIMG) $(INTERNAL_RECOVERYIMAGE_ARGS) $(INTERNAL_MKBOOTIMG_VERSION_ARGS) $(BOARD_MKBOOTIMG_ARGS) --output $@
 	$(hide) echo -n "SEANDROIDENFORCE" >> $@
