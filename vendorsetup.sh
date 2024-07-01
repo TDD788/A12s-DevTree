61556bad9ca777983a93b7d8f000f4223a7b3337
TDD788 <thedarkdeath788@gmail.com>
2024-07-01 00:38:46 -0300

    Updating Every Code

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-# Installation and cloning AVB tool
-sudo apt install nano
-git clone https://gitlab.com/EdwinT2/avb_tool -b main out/host/linux-x86/bin
-sudo chmod +rwx out/host/linux-x86/bin/avbtool
-#chmod a+x device/samsung/a12s/prebuilt/avb/mkbootimg
+chmod a+x device/samsung/a12s/prebuilt/avb/mkbootimg
 add_lunch_combo twrp_a12s-eng
 
 # Device variables
