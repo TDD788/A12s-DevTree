fb5a2c6a82a8aa846a3e99b85b645dacd6e8c27e
TDD788 <thedarkdeath788@gmail.com>
2024-07-09 11:52:10 -0300

    Creating a Branch dedicated to the Community

--- twrp_a12s.mk
+++ twrp_a12s.mk
@@ -19,15 +19,15 @@ DEVICE_PATH := device/samsung/a12s
 # Release name
 PRODUCT_RELEASE_NAME := a12s
 
-# Call Inherit Orange Fox configuration
-$(call inherit-product, $(DEVICE_PATH)/fox_a12s.mk)
-
 # Inherit from common AOSP config
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
+
+# Call Inherit Orange Fox configuration
+$(call inherit-product, $(DEVICE_PATH)/fox_a12s.mk)
 
 # Inherit device configuration
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
