d0b4de8f860b59600740b47ec81c58946889b5fe
TheDarkDeath788 <thedarkdeath788@gmail.com>
2024-08-20 23:30:48 -0300

    Update postrecoveryboot.sh

--- recovery/root/system/bin/postrecoveryboot.sh
+++ recovery/root/system/bin/postrecoveryboot.sh
@@ -4,4 +4,21 @@ if [ -f /sbin/from_fox_sd.sh ]; then
    source /sbin/from_fox_sd.sh
 fi
 
-exit 0
+gsitouchfix() {
+    echo check_connection > /sys/class/sec/tsp/cmd && cat /sys/class/sec/tsp/cmd_result
+}
+
+monitor_events() {
+    while true; do
+        getevent | while read event2; do
+            case "$event2" in
+                *"/dev/input/event2: 0001 0074 00000000"*)
+                    gsitouchfix
+                    ;;
+            esac
+        done
+    done
+}
+
+gsitouchfix
+monitor_events &
