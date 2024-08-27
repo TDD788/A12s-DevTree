e5c6d1c94b758df1b4b0d0a0657b9d2bd45e6c63
TheDarkDeath788 <thedarkdeath788@gmail.com>
2024-08-27 15:32:53 -0300

    Update twrp_a12s.mk

--- twrp_a12s.mk
+++ twrp_a12s.mk
@@ -22,6 +22,7 @@ PRODUCT_RELEASE_NAME := a12s
 # Inherit from common AOSP config
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common TWRP stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
